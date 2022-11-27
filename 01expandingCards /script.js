93bc243d1b3a37c6c273729873027bdcbfb802c7
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-11-27 15:47:52 -0300

    adding comments

--- 01expandingCards /script.js
+++ 01expandingCards /script.js
@@ -1,12 +1,13 @@
 const panels = document.querySelectorAll('.panel')
 
+// add an event listener on every panel. Add an active class.
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
-
+//remove the class that was active.
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
--- 02progressSteps/script.js
+++ 02progressSteps/script.js
@@ -1,49 +1,56 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-let currentActive = 1
+let currentActive = 1;
 
-next.addEventListener('click', () => {
-    currentActive++
+next.addEventListener("click", () => {
+  currentActive++;
+  console.log(currentActive);
 
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
 
-    update()
-})
+  update();
+});
 
-prev.addEventListener('click', () => {
-    currentActive--
+prev.addEventListener("click", () => {
+  currentActive--;
+  console.log(currentActive);
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
 
-    if(currentActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
+  update();
+});
 
 function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
+  //update Active class in circle
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
     } else {
-        prev.disabled = false
-        next.disabled = false
+      circle.classList.remove("active");
     }
+  });
+
+  //update the progress bar percentage 
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  //change the button enabeled and disabled state
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
